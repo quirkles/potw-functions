861bd7b7643211d07d9b76855d659f7b9dfac422
quirkles <al.quirk@gmail.com>
2024-03-27 17:10:21 -0400

    Add include me option

--- functions/src/games/createGame.ts
+++ functions/src/games/createGame.ts
@@ -3,24 +3,36 @@ import {getDb} from "../db/dbClient";
 import {z} from "zod";
 import {games} from "../db/schema/game";
 import {users} from "../db/schema/user";
+import {gamesToUsers} from "../db/schema/games_to_users";
 
 const createGamePayloadSchema = z.object({
   name: z.string(),
   description: z.string().optional(),
   isPrivate: z.boolean(),
   adminId: z.string(),
+  includeAdminAsPlayer: z.boolean().optional(),
 });
 export const createGame = onRequest(async (req, res) => {
   const db = getDb();
   const validated = createGamePayloadSchema.parse(req.body);
-  const [inserted] = await db.insert(games).values({
-    name: validated.name,
-    description: validated.description,
-    isPrivate: validated.isPrivate,
-    adminId: validated.adminId,
-  }).returning({insertedId: users.id});
+  let newGameId;
+  await db.transaction(async (tx) => {
+    const [inserted] = await tx.insert(games).values({
+      name: validated.name,
+      description: validated.description,
+      isPrivate: validated.isPrivate,
+      adminId: validated.adminId,
+    }).returning({insertedId: users.id});
+    newGameId = inserted.insertedId;
+    if (validated.includeAdminAsPlayer) {
+      await tx.insert(gamesToUsers).values({
+        gameId: newGameId,
+        userId: validated.adminId,
+      });
+    }
+  });
   res.status(201).send({
-    id: inserted.insertedId,
+    id: newGameId,
     ...validated,
   });
 });
